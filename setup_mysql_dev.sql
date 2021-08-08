9f9d8b5ae88608ff4ce53cf8f933bfe25274f412
SebastianMH14 <sebasmh2002@hotmail.com>
2021-08-08 15:53:55 -0500

    fix privileges to setup_dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,6 +3,6 @@
 -- A new user hbnb_dev (in localhost)
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT type_of_permission ON database_name.table_name TO 'username'@'localhost';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost'
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost'
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost'
