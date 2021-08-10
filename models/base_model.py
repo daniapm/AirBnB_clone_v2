ead92229254affd9fb8daf380240d08d1d31812c
daniapm <daniajibellis@hotmail.com>
2021-08-10 14:41:38 -0500

    fix code base_models

--- models/base_model.py
+++ models/base_model.py
@@ -4,9 +4,8 @@ import uuid
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-
+from models import storage
 Base = declarative_base()
-
 
 class BaseModel:
     """A base class for all hbnb models"""
@@ -14,28 +13,26 @@ class BaseModel:
     created_at = Column(DateTime(), nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime(), nullable=False, default=datetime.utcnow())
 
-
-
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            
+            storage.new(self)
         else:
             for key, value in kwargs.items():
-                if key == "__class"__:
+                if key == "__class__":
                     continue
                 elif key == "created_at":
-                    setattr(self, key, datatime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                elif key == "update_at":
-                    setattr(self, key, datatime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    setattr(self, key, datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key == "updated_at":
+                    setattr(self, key, datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f'))
                 else:
-                    setattr(self, key, value) 
-        del kwargs['class']
-        self.dict.update(kwargs)
+                    setattr(self, key, value)
+
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
