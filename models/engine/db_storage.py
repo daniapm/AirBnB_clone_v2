0f38276376c7ab41699101d80e948a3ab5c8cc64
daniapm <daniajibellis@hotmail.com>
2021-08-09 22:40:02 -0500

    fix pep8

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,25 +1,65 @@
 #!/usr/bin/python3
 from sqlalchemy import (create_engine)
 from os import getenv
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
+from models.base_model import Base, BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.state import State
+from models.user import User
+from models.place import Place
+from sqlalchemy.orm import scoped_session
+
 
 class DBStorage:
-	__engine = None
-	__session = None
-	
-	def __init__(self):
-		self.__engine = create_engine ('mysql+mysqldb://{}:{}@localhost/{}'.format(HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_DB)), pool_pre_ping=True)
-		
-	@property
-    def session(self):
-        return self.__session
+    __engine = None
+    __session = None
 
-   	@session.setter
-    def get_session(self, value):
-		self.__session = value
+    def __init__(self):
+        usuario = getenv("HBNB_MYSQL_USER")
+        contraseña = getenv("HBNB_MYSQL_PWD")
+        base_datos = getenv("HBNB_MYSQL_DB")
+        post = getenv("HBNB_MYSQL_HOST")
+        running_environment = ("HBNB_ENV")
 
-	__session = sessionmaker(bind=__engine)
-    __Session = Session()
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                                      .format(usuario, contraseña,
+                                      post, base_datos),
+                                      pool_pre_ping=True)
 
+        if running_environment == "test":
+            Base.metadata.drop_all(self.__engine)
 
-		
+    def all(self, cls=None):
+        new_dict = {}
+        if cls is not None:
+            for key in self.__session.query(cls).all:
+                new_dict.update((key.__class__.__name__ + "." + key.id))
+            return new_dict
+
+        else:
+            new_dict.update(self.__session.query(User).all)
+            new_dict.update(self.__session.query(State).all)
+            new_dict.update(self.__session.query(City).all)
+            new_dict.update(self.__session.query(Amenity).all)
+            new_dict.update(self.__session.query(Place).all)
+            return new_dict
+
+    def new(self, obj):
+        self.__session.add(obj)
+
+    def save(self):
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        if obj is not None:
+            self.session.delete(obj)
+
+    def reload(self):
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        self.__Session = scoped_session(session_factory)
+        self.__Session = Session()
+
+        Session.close()
