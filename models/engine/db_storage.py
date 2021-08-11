90809fbd347d0ed67a1b74cc9f7d3205ce4803fa
daniapm <daniajibellis@hotmail.com>
2021-08-11 10:56:39 -0500

    fix pep8 styles

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""this module defines engine DBStorage"""
 from sqlalchemy import (create_engine)
 from os import getenv
 from sqlalchemy.orm import sessionmaker, Session
@@ -13,20 +14,24 @@ from sqlalchemy.orm import scoped_session
 
 
 class DBStorage:
+    """DBStorage class"""
     __engine = None
     __session = None
 
     def __init__(self):
-
+        """initialization of class DBStorage"""
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                        .format(getenv("HBNB_MYSQL_USER"), getenv("HBNB_MYSQL_PWD"),
-                                        getenv("HBNB_MYSQL_HOST"), getenv("HBNB_MYSQL_DB")),
-                                        pool_pre_ping=True)
+                                      .format(getenv("HBNB_MYSQL_USER"),
+                                              getenv("HBNB_MYSQL_PWD"),
+                                              getenv("HBNB_MYSQL_HOST"),
+                                              getenv("HBNB_MYSQL_DB")),
+                                      pool_pre_ping=True)
 
         if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
-        
+
     def all(self, cls=None):
+        """method that return all objecs"""
         new_dict = {}
         if cls is not None:
             for key in self.__session.query(cls):
@@ -47,16 +52,20 @@ class DBStorage:
             return new_dict
 
     def new(self, obj):
+        """method new that add a new object"""
         self.__session.add(obj)
 
     def save(self):
+        """method save that commit changes"""
         self.__session.commit()
 
     def delete(self, obj=None):
+        """method delete that delete the databade"""
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
+        """method reload that reload the session"""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
