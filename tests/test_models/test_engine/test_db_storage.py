4c18e4a0ee22ced547661b72d56af02e892edbdf
LuisColorado-tech <luiscolorado@outlook.com>
2021-08-11 20:01:03 -0500

    add test

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,63 +1,25 @@
 #!/usr/bin/python3
-""" . """
+""" Module for testing file storage"""
+import unittest
+from models.base_model import BaseModel
+from models import storage
+import os
 from models.engine.db_storage import DBStorage
-from models.engine import db_storage
-from models.__init__ import storage
-from unittest.mock import patch
-from io import StringIO
-import console
-import unittest
-import pep8
-import sys
-from os import getenv
-import MySQLdb
-import models
-import unittest
 
 
-class TestPep8B(unittest.TestCase):
-    """ Check for pep8 validation. """
-    @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', 'can not run file')
-    def test_pep8(self):
-        """ test base and test_base for pep8 conformance """
-        style = pep8.StyleGuide(quiet=True)
-        file1 = 'models/engine/db_storage.py'
-        file2 = 'tests/test_models/test_engine/test_db_storage.py'
-        result = style.check_files([file1, file2])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warning).")
+class testDBStorage(unittest.TestCase):
+    """ Class to test the file db_storage method """
 
+    def test_pep8_base(self):
+        """ test pep8 style"""
+        self.assertEqual(os.system('pep8 models/engine/db_storage.py'), 0)
 
-class TestDocsB(unittest.TestCase):
-    """ Check for documentation. """
-    @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', 'can not run file')
-    def test_module_doc(self):
-        """ check for module documentation """
-        self.assertTrue(len(db_storage.__doc__) > 0)
-
-    @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', 'can not run file')
-    def test_class_doc(self):
-        """ check for documentation """
-        self.assertTrue(len(DBStorage.__doc__) > 0)
-
-    @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', 'can not run file')
-    def test_method_docs(self):
-        """ check for method documentation """
-        for func in dir(DBStorage):
-            self.assertTrue(len(func.__doc__) > 0)
-
-
-@unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', 'can not run file')
-class TestDBStorage(unittest.TestCase):
-    """ Check for functionaly of Console. """
-    def setUp(self):
-        """Setting Up """
-        self.console_o = DBStorage()
-
-    def tearDown(self):
-        """Cleaning up after each test. """
-        pass
-
-if __name__ == "__main__":
-    unittest.main()
-    
+    def test_module_docstring(self):
+        """test documentation"""
+        self.assertTrue(len(DBStorage.__doc__) > 1)
+        self.assertTrue(len(DBStorage.__init__.__doc__) > 1)
+        self.assertTrue(len(DBStorage.all.__doc__) > 1)
+        self.assertTrue(len(DBStorage.save.__doc__) > 1)
+        self.assertTrue(len(DBStorage.new.__doc__) > 1)
+        self.assertTrue(len(DBStorage.delete.__doc__) > 1)
+        self.assertTrue(len(DBStorage.reload.__doc__) > 1)
