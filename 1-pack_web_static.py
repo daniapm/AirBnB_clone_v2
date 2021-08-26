3abe6aa016c22bc2349fa98b86c2fbc884771c06
daniapm <daniajibellis@hotmail.com>
2021-08-26 09:13:14 -0500

    fix pep8 styles

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,6 +4,7 @@ import os.path
 from datetime import datetime
 from fabric.api import local
 
+
 def do_pack():
     """Fabric script that generates a .tgz archive from the contents of the
     web_static folder of your AirBnB Clone repo, using the function do_pack.
