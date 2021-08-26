0f473bd0e27f341b0dc6d116fdf32587487d03aa
daniapm <daniajibellis@hotmail.com>
2021-08-26 09:07:14 -0500

    file that Prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo 'Holberton School' | sudo tee /data/web_static/releases/test/index.html
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
+sudo sed -i '40i\\tlocation /hbnb_static/ {\n\t\alias //data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
