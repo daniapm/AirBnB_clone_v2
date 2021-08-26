02f1c3d8fdc401767334a363bd9c92827b585521
daniapm <daniajibellis@hotmail.com>
2021-08-26 15:32:45 -0500

    fix pep8 styles

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,6 +6,7 @@ from fabric.api import put, run, env
 
 env.hosts = ['35.196.239.113', '3.94.160.0']
 
+
 def do_deploy(archive_path):
     """
     Deply funtion that Deploy archive!
@@ -17,13 +18,16 @@ def do_deploy(archive_path):
         file_new = file_old.split(".")
         put(archive_path, "/tmp/".format(file_old))
         run('mkdir -p /data/web_static/releases/{}/'.format(file_new[0]))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(file_old, file_new[0]))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(file_old, file_new[0]))
         run('rm /tmp/{}'.format(file_old))
         run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(file_new[0], file_new[0]))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(file_new[0]))
+            "/data/web_static/releases/{}/".format(file_new[0], file_new[0]))
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(file_new[0]))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(file_new[0]))
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(file_new[0]))
         return True
     except:
         return False
