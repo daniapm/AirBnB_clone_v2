0d90b47302b10b9a1fb3476e9955d66feac96079
daniapm <daniajibellis@hotmail.com>
2021-09-12 18:49:25 -0500

    adding comment

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""
+script that starts a Flask web application:
+"""
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
 
+@app.route('/', strict_slashes=False)
 def hello_world():
+    """
+    print Hello HBNB!
+    """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
