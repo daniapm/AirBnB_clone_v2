869e06ad7eebd7adf1df9935ee9c327c6f7a2381
daniapm <daniajibellis@hotmail.com>
2021-09-12 20:05:36 -0500

    fix code}

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -32,9 +32,9 @@ def hello_c(text):
     return 'C {}'.format(text)
 
 
-@app.route('/python', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/(<text>)', strict_slashes=False)
-def hello_python(text="is cool"):
+def hello_python(text='is cool'):
     """
     display “Python ”, followed by the value of the text variable
     """
