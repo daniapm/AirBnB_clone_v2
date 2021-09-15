fcf79f4823d0314cdf0555300f80ec06e60c2cc2
daniapm <daniajibellis@hotmail.com>
2021-09-15 10:05:33 -0500

    fix code

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
 """
-script that starts a Flask web application:
+    Sript that starts a Flask web application
 """
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 from models import storage
+from models.state import State
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def handle_teardown(self):
-    """
-    Teardown events abort
-    """
-    storage.close()
-
 
 @app.route('/states_list', strict_slashes=False)
 def state_list():
     """
-    Display a HTML page
+        method to render states
     """
-    states = storage.all('State').values()
+    states = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """
+        method to handle teardown
+    """
+    storage.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
