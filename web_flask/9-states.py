0b26d60661667ca6da17ead34c5c9d3b6956a61e
daniapm <daniajibellis@hotmail.com>
2021-09-15 16:32:25 -0500

    States and State

---
+++ web_flask/9-states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    Sript that starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route('/states/<id>')
+@app.route('/states', strict_slashes=False)
+def states_id(id=None):
+    """
+        method to render states
+    """
+    states = storage.all(State)
+    if id:
+        id = "State." + id
+    return render_template("9-states.html", states=states, id=id)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """
+        method to handle teardown
+    """
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
